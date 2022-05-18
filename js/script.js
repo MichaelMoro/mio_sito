343832ab37dbea4db78d772ab1ebf872d5d0adb9
Michael Moro <michael.moro@lynx.italy>
2022-05-18 12:21:46 +0200

    PaginaSingolo+insta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 var alimentazioneHTML = "contenuti/alimentazione.html";
 var homeHTML = "contenuti/home.html";
+var crossfitHTML = "contenuti/crossfit.html";
+
 
 function apriAlimentazione() {
     $ajaxUtils.sendGetRequest(
@@ -13,16 +15,32 @@ function apriAlimentazione() {
 };
 
 function ritornaHome() {
-
     $ajaxUtils.sendGetRequest(
         homeHTML,
         function (responseText) {
             document.querySelector("#contenuto_pagina_centrale")
                 .innerHTML = responseText;
         })
-
-
-
-
 }
 
+function apriCrossfit() {
+    $ajaxUtils.sendGetRequest(
+        crossfitHTML,
+        function (responseText) {
+            document.querySelector("#contenuto_pagina_centrale")
+                .innerHTML = responseText;
+        })
+}
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // On first load, show home view
+    $ajaxUtils.sendGetRequest(
+        homeHTML,
+        function (responseText) {
+            document.querySelector("#contenuto_pagina_centrale")
+                .innerHTML = responseText;
+        },
+        false);
+});
